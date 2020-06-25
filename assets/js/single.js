dade601b343ad27e6dabc9d97c80257e0e6afe9e
Windy <lauren.euge@gmail.com>
2020-06-24 22:34:37 -0700

    list-issues

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,30 +1,26 @@
 var limitWarningEl = document.querySelector("#limit-warning");
 var issueContainerEl = document.querySelector("#issues-container");
 var repoNameEl = document.querySelector("#repo-name");
+
 var getRepoIssues = function(repo) {
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
     fetch(apiUrl).then(function(response) {
+      fetch(apiUrl).then(function(response) {
         // request was successful
         if (response.ok) {
           response.json().then(function(data) {
-            // pass response data to dom function
             displayIssues(data);
+            // check if api has paginated issues
+            if (response.headers.get("Link")) {
+              displayWarning(repo);
+            }
           });
+        } else {
+          // if not successful, redirect to homepage
+          document.location.replace("./index.html");
         }
-        else {
-          alert("There was a problem with your request!");
-        }
-        if (response.ok) {
-            response.json().then(function(data) {
-              displayIssues(data);
-          
-              // check if api has paginated issues
-              if (response.headers.get("Link")) {
-                displayWarning(repo);
-              }
-            });
-          }
       });
+    });
 
     var displayIssues = function(issues) {
         if (issues.length === 0) {
@@ -74,8 +70,17 @@ var displayWarning = function(repo) {
     };
 
 var getRepoName = function() {
+  // grab repo name from url query string
   var queryString = document.location.search;
   var repoName = queryString.split("=")[1];
-  getRepoIssues(repoName);
-  repoNameEl.textContent = repoName;
-}
+
+  if (repoName) {
+    // display repo name on the page
+    repoNameEl.textContent = repoName;
+
+    getRepoIssues(repoName);
+  } else {
+    // if no repo was given, redirect to the homepage
+    document.location.replace("./index.html");
+  }
+};
